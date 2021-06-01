60c30494e3f31ebb18280735396bf7afe91f597f
GermanShtey <gvshteynardt@gmail.com>
2021-06-01 22:37:05 +0300

    added cache

--- src/app/init/client.js
+++ src/app/init/client.js
@@ -1,7 +1,15 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const uri = 'https://funded-pet-library.moonhighway.com/';
+const link = createHttpLink({
+  uri,
+});
+
+const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
-  uri,
+  cache,
+  link,
 });

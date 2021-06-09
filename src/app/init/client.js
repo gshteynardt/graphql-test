6401e167a989f2ecddf84a8ad0cbbaa5ba495b49
GermanShtey <gvshteynardt@gmail.com>
2021-06-09 18:50:33 +0300

    added files

--- src/app/init/client.js
+++ src/app/init/client.js
@@ -1,11 +1,20 @@
 import ApolloClient from 'apollo-client';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
 
-const uri = 'https://funded-pet-library.moonhighway.com/';
+const ROOT = 'funded-pet-library.moonhighway.com/';
+const uri = `https://${ROOT}`;
 const httpLink = createHttpLink({
   uri,
+});
+
+const wsLink = new WebSocketLink({
+  uri: `wss://${ROOT}/graphql`,
+  options: { reconnect: true },
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -19,9 +28,24 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wrappedHttpLink = authLink.concat(httpLink);
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 
+        'OperationDefinition' && 
+        definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  wrappedHttpLink,
+);
+
 const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
   cache,
-  link: authLink.concat(httpLink),
+  link,
 });
